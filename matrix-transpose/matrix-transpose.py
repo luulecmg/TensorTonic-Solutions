23a23e7637fbd3e6c954433a0aeb0d36421b89f3
luule.cmg <leluu.cmg@gmail.com>
2026-04-30 13:42:00 +0700

    Add solution for matrix-transpose

---
+++ matrix-transpose/matrix-transpose.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def matrix_transpose(A):
+    """
+    Return the transpose of matrix A (swap rows and columns).
+    """
+    # Write code here
+    A = np.array(A)
+    H, W = A.shape
+    B = np.empty((W, H))
+
+    for i in range(H):
+        for j in range(W):
+            B[j, i] = A[i, j]
+
+    return B
+
+    
